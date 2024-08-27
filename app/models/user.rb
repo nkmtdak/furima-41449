44f28d2b452cb6a548a456063182c7b9ea068582
nkmtdak <tanemaki@hotmail.com>
2024-08-27 18:37:46 +0900

    テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,13 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :family_name, presence: true
   validates :email, presence: true
-
   validates :birth_date, presence: true
-    # メールアドレスが必須、一意性、@を含む
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i ,}
 
-  # パスワード関連のバリデーション
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i ,}
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: ' is invalid. Include both letters and numbers' }
 
   # お名前(全角)のバリデーション
   validates :family_name, :first_name, presence: true
-  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ ,}
+  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,}
 
   # お名前カナ(全角)のバリデーション
   validates :family_name_kana, :first_name_kana, presence: true
