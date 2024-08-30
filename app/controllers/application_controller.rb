c033435c4a5c1aca71003161e98151cdc8f06b71
nkmtdak <tanemaki@hotmail.com>
2024-08-30 14:40:35 +0900

    一覧表示修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :password, :password_confirmation, :family_name, :first_name,
-                                             :family_name_kana, :first_name_kana, :birth_date])
+                                      keys: [:nickname, :email, :password, :password_confirmation, :family_name, :first_name, :family_name_kana, :first_name_kana,
+                                             :birth_date])
   end
 end
